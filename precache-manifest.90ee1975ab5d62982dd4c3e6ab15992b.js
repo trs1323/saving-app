12381ba8d0180f210900ad9f06b6f5a2953ab5a9
trs1323 <45774674+trs1323@users.noreply.github.com>
2020-03-27 15:33:16 -0700

    Updates

---
+++ precache-manifest.90ee1975ab5d62982dd4c3e6ab15992b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d8f11fe89c68fd42479db0ea21c5a4",
+    "url": "/saving-app/index.html"
+  },
+  {
+    "revision": "40c1d1f84b06ebde3b1f",
+    "url": "/saving-app/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "f8c551443d1ebf8f50d4",
+    "url": "/saving-app/static/css/main.63ac4f19.chunk.css"
+  },
+  {
+    "revision": "40c1d1f84b06ebde3b1f",
+    "url": "/saving-app/static/js/2.fec97e81.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/saving-app/static/js/2.fec97e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c551443d1ebf8f50d4",
+    "url": "/saving-app/static/js/main.cfe34c81.chunk.js"
+  },
+  {
+    "revision": "0d9bd66e656ec00603bb",
+    "url": "/saving-app/static/js/runtime-main.7d87adf6.js"
+  },
+  {
+    "revision": "e67ae2ab91be6b287f57d774f25259f6",
+    "url": "/saving-app/static/media/family.e67ae2ab.png"
+  },
+  {
+    "revision": "370dc5e471661a53d66b1ae88efe867a",
+    "url": "/saving-app/static/media/fullheadshot.370dc5e4.png"
+  },
+  {
+    "revision": "c7d25f9d48be9ed99acd93373bc921c2",
+    "url": "/saving-app/static/media/headshot-1.c7d25f9d.png"
+  },
+  {
+    "revision": "bcc8e36c8d82f7e9ad5d966ad19e683c",
+    "url": "/saving-app/static/media/headshot-2.bcc8e36c.png"
+  },
+  {
+    "revision": "b0be4a0ea5c5081657ec9f594c8f619e",
+    "url": "/saving-app/static/media/headshot-3.b0be4a0e.png"
+  },
+  {
+    "revision": "4060b2d0669f57b7fc8e126e62bd37b7",
+    "url": "/saving-app/static/media/headshot-4.4060b2d0.png"
+  },
+  {
+    "revision": "00dd67f9bbbfff852b382b4b166ae67d",
+    "url": "/saving-app/static/media/headshot-5.00dd67f9.png"
+  },
+  {
+    "revision": "99805a9cb2f647ff78b7f1fdf06cefe8",
+    "url": "/saving-app/static/media/oneofakind.99805a9c.jpg"
+  }
+]);
